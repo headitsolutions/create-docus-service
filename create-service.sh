28386ba7ccfad1a6a349219ce797f06a1fd1b7f0
Jonas Suter <jonas.suter@fhnw.ch>
2023-06-20 13:12:25 +0000

    added file

---
+++ create-service.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Replace "your-service-name" with the desired name for your service
+SERVICE_NAME="docus"
+
+# Replace "/path/to/your/service" with the actual path to your service executable
+SERVICE_PATH="/usr/bin/dotnet /var/www/api/Docus.WebApi.Host.dll --environment Rele"
+
+# Replace "your-username" with your username (the user who will run the service)
+SERVICE_USER="root"
+
+# Replace "/path/to/log/file.log" with the path to the log file for your service
+LOG_FILE="/var/log/docus.log"
+
+# Create a service file in the systemd directory
+SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
+
+# Write the service unit file
+cat > $SERVICE_FILE <<EOF
+[Unit]
+Description=Docus
+
+[Service]
+ExecStart=$SERVICE_PATH
+WorkingDirectory=/var/www/api/
+SyslogIdentifier=$SERVICE_NAME
+User=$SERVICE_USER
+
+Environment=DOTNET_ROOT=/usr/lib64/dotnet
+
+# Always restart the service
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Set correct permissions for the service file
+chmod 644 $SERVICE_FILE
+
+# Reload systemd daemon
+systemctl daemon-reload
+
+# Enable the service to start on boot
+systemctl enable $SERVICE_NAME
+
+# Start the service
+systemctl start $SERVICE_NAME
+
+# Check the status of the service
+systemctl status $SERVICE_NAME
+
+# View the service log
+journalctl -u $SERVICE_NAME -f
